bbedc61d5daf7070d21bf97e6f8b76b3c3a5eaf2
Santhosh Kyasa <30811886+skyasa@users.noreply.github.com>
2023-09-12 21:20:55 -0500

    Update streamlit_app.py
    remove text

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega3 & blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
